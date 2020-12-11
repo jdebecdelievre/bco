added safeguards for bjorck with normalization, fixed output mean

bco/train.py
@@ -78,6 +78,10 @@ def process_params(params, dest_dir):
     torch.manual_seed(params['seed'])
     np.random.seed(params['seed'])
 
+    # Data normalization is not possible with Bjorck layers because they guarnatee a 
+    # Lipshitz constant of exactly one 
+    if params['normalize_input'] or params['normalize_output']:
+        assert params['model']['linear'] != 'bjorck', "Bjorck layers incompatible with data normalization"
     return params
 
 def train(params={}, tune_search=False, dest_dir='.'):
@@ -98,7 +102,7 @@ def train(params={}, tune_search=False, dest_dir='.'):
         model.input_mean.data = dataset.input_mean.data
         model.input_std.data = dataset.input_std.data
     if params['normalize_output']:
-        model.output_mean.data = dataset.output_mean.data
+        model.output_mean.data = dataset.output_mean.data*0.
         model.output_std.data = dataset.output_std.data
  
 

bco/training/datasets.py
@@ -68,9 +68,15 @@ class BaseDataset():
         # define input and output mean
         self.input_mean = (ifs.sum(0) + fs.sum(0)) / self.n_total
         self.output_mean = out.mean(0)
-        self.input_std = ((ifs - self.input_mean).square().sum(0) + \
-                            (fs - self.input_mean).square().sum(0)).div(self.n_total - 1).sqrt()
-        self.output_std = out.std(0)
+        if self.n_total > 1:
+            self.input_std = ((ifs - self.input_mean).square().sum(0) + \
+                                (fs - self.input_mean).square().sum(0)).div(self.n_total - 1).sqrt()
+        else:
+            self.input_std = torch.ones(ifs.shape(1))
+        if self.n_infeasible > 1:
+            self.output_std = out.std(0)
+        else:
+            self.output_std = torch.ones(ifs.shape(1))
 
         # batching
         n_slices = np.maximum( (self.n_feasible + self.n_infeasible) // params['optim']['batch_size'], 1)